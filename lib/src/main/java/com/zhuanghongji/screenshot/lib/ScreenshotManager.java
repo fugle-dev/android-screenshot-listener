5a5671c4902b82e59bac57e9d872dd5feafce3db
zhuanghongji <zhuanghongjichina@gmail.com>
2018-05-13 14:32:54 +0800

    修正在小米手机监听截图的事件问题

--- lib/src/main/java/com/zhuanghongji/screenshot/lib/ScreenshotManager.java
+++ lib/src/main/java/com/zhuanghongji/screenshot/lib/ScreenshotManager.java
@@ -14,7 +14,7 @@ import java.util.List;
 /**
  * the manager of screenshot.
  * <p>
- *     Please make sure you have the permission of reading external storage.
+ * Please make sure you have the permission of reading external storage.
  * <p/>
  *
  * @author zhuanghongji
@@ -30,6 +30,8 @@ public class ScreenshotManager {
      * it make the {@link OnScreenshotListener#onScreenshot(String)} execute on ui thread.
      */
     private Handler mHandler;
+
+    private List<Integer> mScreenshotEvents;
 
     /**
      * the collection of screenshot dir on most brand mobile-phone.
@@ -47,9 +49,20 @@ public class ScreenshotManager {
     public ScreenshotManager(Context context) {
         mContext = context;
         mHandler = new Handler();
+        mScreenshotEvents = new ArrayList<>();
         mScreenshotDirectories = new ArrayList<>();
         mFileObservers = new ArrayList<>();
+
+        initScreenshotEvents();
         initScreenshotDirectories();
+    }
+
+    /**
+     * here we assume CLOSE_WRITE/CLOSE_NOWRITE event is screenshot event
+     */
+    private void initScreenshotEvents() {
+        mScreenshotEvents.add(FileObserver.CLOSE_WRITE);
+        mScreenshotEvents.add(FileObserver.CLOSE_NOWRITE);
     }
 
     private void initScreenshotDirectories() {
@@ -73,6 +86,7 @@ public class ScreenshotManager {
     /**
      * if you want to listen dir witch didn't add in {@link #initScreenshotDirectories()}, you can
      * add it by this method.
+     *
      * @param screenshotDir the dir you want to listen
      */
     public void addScreenshotDirectories(final String screenshotDir) {
@@ -80,10 +94,13 @@ public class ScreenshotManager {
         mFileObservers.add(new FileObserver(screenshotDir, FileObserver.ALL_EVENTS) {
             @Override
             public void onEvent(int event, @Nullable final String path) {
+                final String desc = getEventDesc(event);
                 final String absolutePath = screenshotDir + File.separator + path;
-                Log.v(TAG, "event = " + event + ", absolutePath = " + absolutePath);
+                Log.v(TAG, "event = " + event
+                        + ", desc = " + desc
+                        + ", absolutePath = " + absolutePath);
 
-                if (event == FileObserver.CREATE) {
+                if (mScreenshotEvents.contains(event)) {
                     mHandler.post(new Runnable() {
                         @Override
                         public void run() {
@@ -95,6 +112,58 @@ public class ScreenshotManager {
                 }
             }
         });
+    }
+
+    /**
+     * get the description of FileObserver event
+     * @param event the event for match description
+     * @return the description string of event
+     */
+    private String getEventDesc(int event) {
+        String desc;
+        switch (event) {
+            case FileObserver.ACCESS:
+                desc = "ACCESS: Data was read from a file";
+                break;
+            case FileObserver.MODIFY:
+                desc = "MODIFY: Data was written to a file";
+                break;
+            case FileObserver.ATTRIB:
+                desc = "ATTRIB: Metadata (permissions, owner, timestamp) was changed explicitly";
+                break;
+            case FileObserver.CLOSE_WRITE:
+                desc = "CLOSE_WRITE: Someone had a file or directory open for writing, and closed it";
+                break;
+            case FileObserver.CLOSE_NOWRITE:
+                desc = "CLOSE_NOWRITE: Someone had a file or directory open read-only, and closed it";
+                break;
+            case FileObserver.OPEN:
+                desc = "OPEN: A file or directory was opened";
+                break;
+            case FileObserver.MOVED_FROM:
+                desc = "MOVED_FROM: A file or subdirectory was moved from the monitored directory";
+                break;
+            case FileObserver.MOVED_TO:
+                desc = "MOVED_TO: A file or subdirectory was moved to the monitored directory";
+                break;
+            case FileObserver.CREATE:
+                desc = "CREATE: A new file or subdirectory was created under the monitored directory";
+                break;
+            case FileObserver.DELETE:
+                desc = "DELETE: A file was deleted from the monitored directory";
+                break;
+            case FileObserver.DELETE_SELF:
+                desc = "DELETE_SELF: The monitored file or directory was deleted; monitoring effectively stops";
+                break;
+            case FileObserver.MOVE_SELF:
+                desc = "MOVE_SELF: The monitored file or directory was moved; monitoring continues";
+                break;
+            default:
+                desc = "No match event";
+                break;
+
+        }
+        return desc;
     }
 
     /**
@@ -116,6 +185,15 @@ public class ScreenshotManager {
         }
     }
 
+    /**
+     * add the event of {@link FileObserver} which you think it's screenshot event but not init
+     * in {@link #initScreenshotEvents()}
+     * @param event the event you want to add
+     */
+    public void addScreenshotEvent(int event) {
+        mScreenshotEvents.add(event);
+    }
+
     public List<String> getScreenshotDirectories() {
         return mScreenshotDirectories;
     }
@@ -131,6 +209,7 @@ public class ScreenshotManager {
 
         /**
          * it will called if screenshot event happened.
+         *
          * @param absolutePath the absolutePath of screenshot image file
          */
         void onScreenshot(@Nullable String absolutePath);

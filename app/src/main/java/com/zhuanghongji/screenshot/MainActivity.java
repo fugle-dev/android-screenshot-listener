14fbf60197f7ac32c8114aac6ed88ce43288c91c
nick chen <nick45chen@gmail.com>
2021-11-18 17:17:46 +0800

    removed useless layout

--- app/src/main/java/com/zhuanghongji/screenshot/MainActivity.java
+++ app/src/main/java/com/zhuanghongji/screenshot/MainActivity.java
@@ -1,17 +1,45 @@
 package com.zhuanghongji.screenshot;
 
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.zhuanghongji.screenshot.lib.OnScreenshotListener;
+import com.zhuanghongji.screenshot.lib.ScreenshotManager;
 
 public class MainActivity extends BaseActivity {
+
+    final static String TAG = "MainActivity";
+    ScreenshotManager screenshotManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        screenshotManager = new ScreenshotManager(this);
+        screenshotManager.setOnScreenshotListener(new OnScreenshotListener() {
+            @Override
+            public void onScreenshot(@Nullable String absolutePath) {
+                log("absolutePath: " + absolutePath);
+            }
+        });
     }
 
-    public void gotoSecondActivity(View view) {
-        SecondActivity.actionStart(this);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        screenshotManager.startListen();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        screenshotManager.stopListen();
+    }
+
+    private void log(String msg) {
+        Log.d(TAG, "#### 🤖 " + msg);
     }
 }
